42b73c907ab63c87b8cf57fc729b9cb3cdbea5d6
mamine <mabdelouahab@msn.com>
2022-04-13 20:10:16 +0100

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,26 @@
+include main.inc
+.code
+; -I /usr/include/uasm
+	main PROC SYSTEMV      _argc:DWORD, _argv:QWORD 
+		local largc:DWORD
+		local  largv:QWORD   
+
+
+		mov  largc,_argc    
+		mov  largv,_argv   
+
+		
+		invoke gInit
+		.if largc > 1 
+			mov rax,largv
+			add rax,sizeof gpointer
+			mov rax,[rax]
+			mov Arg_Project,rax 
+		.endif
+		
+		invoke InitMainWindow                
+		invoke gtk_main
+		xor rax,rax
+		ret
+	main ENDP
+	end 
